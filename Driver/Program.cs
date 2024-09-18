248b21828ce32127278902dca556c27c2697b3c6
Ludovic <80395681+Lxdovic@users.noreply.github.com>
2024-09-18 17:50:04 +0200

    Chore/init (#4)
    * chore: init clover
    
    * chore: init tests
    
    * chore: add .idea/ to gitignore
    
    * chore: init driver
    
    * chore: init driver tests
    
    * chore: init

--- Driver/Program.cs
+++ Driver/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace Driver;
+
+internal static class Program {
+    private static void Main(string[] args) {
+        new Driver().Run(args);
+    }
+}
