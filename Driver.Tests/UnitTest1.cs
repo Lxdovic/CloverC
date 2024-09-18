2e01ab65edf9875a249e8729fa3263a9f256d735
Ludovic <80395681+Lxdovic@users.noreply.github.com>
2024-09-18 17:48:05 +0200

    Chore/init (#3)
    * chore: init clover
    
    * chore: init tests
    
    * chore: add .idea/ to gitignore
    
    * chore: init driver
    
    * chore: init driver tests

---
+++ Driver.Tests/UnitTest1.cs
@@ -0,0 +1,6 @@
+namespace Driver.Tests;
+
+public class UnitTest1 {
+    [Fact]
+    public void Test1() { }
+}
---
+++ Driver/Driver.cs
@@ -0,0 +1,17 @@
+﻿namespace Driver;
+
+public sealed class Driver {
+    private static void Error(string message) {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Environment.Exit(1);
+    }
+
+    public void Run(string[] args) {
+        if (args.Length == 0) Error("Usage: clover <source-file>");
+
+        var filePath = args[0];
+
+        if (!Path.Exists(filePath)) Error($"File '{filePath}' does not exist.");
+    }
+}
