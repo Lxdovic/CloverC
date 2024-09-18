b0219a432637e449d837cb2fb4adfd290a9cb990
Lxdovic <ludovicdebever0@gmail.com>
2024-09-18 18:27:21 +0200

    test(driver): added test for taking input

--- Driver.Tests/DriverTests.cs
+++ Driver.Tests/DriverTests.cs
@@ -1,9 +1,26 @@
+using Xunit.Abstractions;
+
 namespace Driver.Tests;
 
 public class DriverTests {
+    private readonly ITestOutputHelper _output;
+
+    public DriverTests(ITestOutputHelper output) {
+        _output = output;
+    }
+
     [Fact]
     public void DriverTakesInput() {
         var driver = new Driver();
-        driver.Run(["test.c"]);
+        var filePath = Path.Combine(Environment.CurrentDirectory, "Resources/input/tet.c");
+
+        using var sw = new StringWriter();
+
+        Console.SetOut(sw);
+
+        var exception = Record.Exception(() => driver.Run([filePath]));
+
+        Assert.Null(exception);
+        Assert.Equal(0, Environment.ExitCode);
     }
 }
