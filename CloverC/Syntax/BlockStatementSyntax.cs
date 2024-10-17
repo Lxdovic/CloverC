076d0b335eb8f419b09ee02f5744564df0b1d722
Lxdovic <ludovicdebever0@gmail.com>
2024-10-17 15:35:00 +0200

    fix(parser): remove unnecessary tokens from blocks and functions

--- CloverC/Syntax/BlockStatementSyntax.cs
+++ CloverC/Syntax/BlockStatementSyntax.cs
@@ -3,12 +3,8 @@ using System.Collections.Immutable;
 namespace CloverC.Syntax;
 
 public sealed class BlockStatementSyntax(
-    SyntaxToken openBraceToken,
-    ImmutableArray<StatementSyntax> statements,
-    SyntaxToken closeBraceToken) : StatementSyntax {
-    public SyntaxToken OpenBraceToken { get; } = openBraceToken;
+    ImmutableArray<StatementSyntax> statements) : StatementSyntax {
     public ImmutableArray<StatementSyntax> Statements { get; } = statements;
-    public SyntaxToken CloseBraceToken { get; } = closeBraceToken;
 
     public override SyntaxKind Kind { get; } = SyntaxKind.BlockStatement;
 }
