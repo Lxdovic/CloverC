7beecba369a82ef317fb92357ebc72c0df746dbb
Lxdovic <ludovicdebever0@gmail.com>
2024-10-17 15:35:48 +0200

    fix(ast-printer): fix display

--- CloverC/Helpers/SyntaxTreePrinter.cs
+++ CloverC/Helpers/SyntaxTreePrinter.cs
@@ -5,28 +5,32 @@ namespace CloverC.Helpers;
 
 public static class SyntaxTreePrinter {
     public static void Print(SyntaxTree syntaxTree) {
-        foreach (var member in syntaxTree.Root.Members) {
-            var sb = new StringBuilder();
-            PrintMember(sb, member);
+        var sb = new StringBuilder();
 
-            Console.WriteLine(sb.ToString());
-        }
+        sb.Append("Program(\n  members: [\n");
+
+        foreach (var member in syntaxTree.Root.Members) PrintMember(sb, member, 2);
+
+        sb.Append("  ]\n)");
+
+        Console.WriteLine(sb.ToString());
     }
 
-    private static void PrintMember(StringBuilder sb, MemberSyntax member) {
+    private static void PrintMember(StringBuilder sb, MemberSyntax member, int indent) {
         switch (member.Kind) {
             case SyntaxKind.FunctionDeclaration:
-                PrintFunctionDeclaration(sb, member as FunctionDeclarationSyntax);
+                PrintFunctionDeclaration(sb, member as FunctionDeclarationSyntax, indent);
                 break;
         }
     }
 
-    private static void PrintFunctionDeclaration(StringBuilder sb, FunctionDeclarationSyntax? function) {
+    private static void PrintFunctionDeclaration(StringBuilder sb, FunctionDeclarationSyntax? function, int indent) {
         if (function == null) return;
 
-        sb.Append($"Function(\n  identifier={function.Identifier.Value},\n  body=");
-        PrintBlockStatement(sb, function.Body, 2);
-        sb.Append("\n)");
+        sb.Append(
+            $"{new string(' ', indent * 2)}Function(\n{new string(' ', (indent + 1) * 2)}identifier={function.Identifier.Value}\n{new string(' ', (indent + 1) * 2)}body=");
+        PrintBlockStatement(sb, function.Body, indent + 2);
+        sb.Append($"\n{new string(' ', indent * 2)})\n");
     }
 
     private static void PrintBlockStatement(StringBuilder sb, BlockStatementSyntax body, int indent) {
