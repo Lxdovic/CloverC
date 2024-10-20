49b6e4bdf555f0a82fa5af447e3791281077ba44
Lxdovic <ludovicdebever0@gmail.com>
2024-10-20 15:23:37 +0200

    fix(stPrinter): fix new line on different envs

--- CloverC/Helpers/SyntaxTreePrinter.cs
+++ CloverC/Helpers/SyntaxTreePrinter.cs
@@ -4,24 +4,26 @@ using CloverC.Syntax;
 namespace CloverC.Helpers;
 
 public static class SyntaxTreePrinter {
+    private static readonly string Nl = Environment.NewLine;
+    
     public static void Print(SyntaxTree syntaxTree) {
         var sb = new StringBuilder();
 
-        sb.Append("Program(\n  members: [\n");
+        sb.Append($"Program({Nl}  members: [{Nl}");
 
         foreach (var member in syntaxTree.Root.Members) PrintMember(sb, member, 2);
 
-        sb.Append("  ]\n)");
+        sb.Append($"  ]{Nl})");
 
         Console.WriteLine(sb.ToString());
     }
     
     public static void Print(SyntaxTree syntaxTree, StringBuilder sb) {
-        sb.Append("Program(\n  members: [\n");
+        sb.Append($"Program({Nl}  members: [{Nl}");
 
         foreach (var member in syntaxTree.Root.Members) PrintMember(sb, member, 2);
 
-        sb.Append("  ]\n)");
+        sb.Append($"  ]{Nl})");
 
         Console.WriteLine(sb.ToString());
     }
@@ -38,22 +40,22 @@ public static class SyntaxTreePrinter {
         if (function == null) return;
 
         sb.Append(
-            $"{new string(' ', indent * 2)}Function(\n{new string(' ', (indent + 1) * 2)}identifier={function.Identifier.Value}\n{new string(' ', (indent + 1) * 2)}body=");
+            $"{new string(' ', indent * 2)}Function({Nl}{new string(' ', (indent + 1) * 2)}identifier={function.Identifier.Value}{Nl}{new string(' ', (indent + 1) * 2)}body=");
         PrintBlockStatement(sb, function.Body, indent + 2);
-        sb.Append($"\n{new string(' ', indent * 2)})\n");
+        sb.Append($"{Nl}{new string(' ', indent * 2)}){Nl}");
     }
 
     private static void PrintBlockStatement(StringBuilder sb, BlockStatementSyntax body, int indent) {
-        sb.Append($"Block(\n{new string(' ', indent * 2)}statements: [");
+        sb.Append($"Block({Nl}{new string(' ', indent * 2)}statements: [");
 
         foreach (var statement in body.Statements) PrintStatementSyntax(sb, statement, indent + 1);
 
-        sb.Append($"\n{new string(' ', indent * 2)}]");
-        sb.Append($"\n{new string(' ', (indent - 1) * 2)})");
+        sb.Append($"{Nl}{new string(' ', indent * 2)}]");
+        sb.Append($"{Nl}{new string(' ', (indent - 1) * 2)})");
     }
 
     private static void PrintStatementSyntax(StringBuilder sb, StatementSyntax statement, int indent) {
-        sb.Append($"\n{new string(' ', indent * 2)}");
+        sb.Append($"{Nl}{new string(' ', indent * 2)}");
 
         switch (statement.Kind) {
             case SyntaxKind.ReturnStatement:
@@ -65,11 +67,11 @@ public static class SyntaxTreePrinter {
     private static void PrintReturnStatement(StringBuilder sb, ReturnStatementSyntax? statement, int indent) {
         if (statement == null) return;
 
-        sb.Append("Return(\n");
+        sb.Append($"Return({Nl}");
 
         if (statement.Expression != null) PrintConstantSyntax(sb, (ConstantSyntax)statement.Expression, indent + 1);
 
-        sb.Append($"\n{new string(' ', indent * 2)})");
+        sb.Append($"{Nl}{new string(' ', indent * 2)})");
     }
 
     private static void PrintConstantSyntax(StringBuilder sb, ConstantSyntax constantStatement, int indent) {
